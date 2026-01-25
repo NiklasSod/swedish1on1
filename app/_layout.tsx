b5e189b087045c315eec8d6143967cbedd7758f0
NiklasSod <nma.prob@gmail.com>
2026-01-25 22:04:33 +0100

    fix for status background when changing orient of phone

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#d3effeff" }}>
       <StatusBar 
         barStyle="dark-content"
         backgroundColor="#d3effeff"
