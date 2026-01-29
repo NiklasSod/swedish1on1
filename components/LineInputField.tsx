2e9e29853a47d380d7cb3e491edae775e38775e3
NiklasSod <nma.prob@gmail.com>
2026-01-29 01:55:07 +0100

    add eye to password input

--- components/LineInputField.tsx
+++ components/LineInputField.tsx
@@ -1,5 +1,8 @@
-import { TextInput, View } from "react-native";
+import { TextInput, View, TouchableOpacity } from "react-native";
 import { styles } from "../styles/lineInputField.styles";
+import { Ionicons } from "@expo/vector-icons";
+import { useState } from "react";
+import { COLORS } from "@/styles/colors";
 
 interface LineInputProps {
   placeholder: string;
@@ -10,16 +13,28 @@ export const LineInput = ({
   placeholder,
   secureTextEntry = false,
 }: LineInputProps) => {
+  const [isHidden, setIsHidden] = useState(secureTextEntry);
+
   return (
     <View style={styles.container}>
       <TextInput
         placeholder={placeholder}
         placeholderTextColor="#999"
-        secureTextEntry={secureTextEntry}
+        secureTextEntry={isHidden}
         style={styles.input}
       />
+      {secureTextEntry && (
+        <TouchableOpacity
+          style={styles.iconContainer}
+          onPress={() => setIsHidden((prev) => !prev)} // better approach if it works
+        >
+          <Ionicons
+            name={isHidden ? "eye-off-outline" : "eye-outline"}
+            size={20}
+            color={COLORS.colors.grayDark}
+          />
+        </TouchableOpacity>
+      )}
     </View>
   );
 };
-
-// add "eye" icon to toggle password visibility
--- styles/lineInputField.styles.ts
+++ styles/lineInputField.styles.ts
@@ -1,15 +1,22 @@
 import { StyleSheet } from "react-native";
+import { COLORS } from "@/styles/colors";
 
 export const styles = StyleSheet.create({
   container: {
     width: "100%",
     marginBottom: 24,
+    flexDirection: "row",
+    alignItems: "center",
+    borderBottomWidth: 1,
+    borderBottomColor: COLORS.colors.grayDark,
   },
   input: {
+    flex: 1,
     height: 40,
     fontSize: 16,
-    borderBottomWidth: 1,
-    borderBottomColor: "#ccc",
     paddingVertical: 8,
+  },
+  iconContainer: {
+    padding: 8,
   },
 });
